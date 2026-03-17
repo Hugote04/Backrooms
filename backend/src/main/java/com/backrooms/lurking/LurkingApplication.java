967d408786378c477b6baa37ea59d1c4260eb634
Hugote04 <marinohugo07@gmail.com>
2026-03-17 18:24:01 +0100

    backend spring boot configurado

---
+++ backend/src/main/java/com/backrooms/lurking/LurkingApplication.java
@@ -0,0 +1,13 @@
+package com.backrooms.lurking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LurkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LurkingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/backrooms/lurking/LurkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backrooms.lurking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LurkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
