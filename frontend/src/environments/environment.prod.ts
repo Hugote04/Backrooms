ca074864e1086f12dc4ce004b1ef1c7a661956c3
Hugote04 <marinohugo07@gmail.com>
2026-05-07 12:47:46 +0200

    deploy del backend en render

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://welxsoivrvfuaavjbeuh.supabase.co',
   supabaseAnonKey: 'sb_publishable_azwEQUmNd5lrFy0YIntgOg_KTDvP_bq',
-  apiUrl: 'https://backrooms-api.onrender.com/api',
+  apiUrl: 'https://backrooms-33o0.onrender.com/api',
 };
